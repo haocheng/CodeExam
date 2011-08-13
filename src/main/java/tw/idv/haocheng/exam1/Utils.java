d0fee88c8985d27a27676ef6be06dc2b3d696448
haocheng <haocheng.lee@gmail.com>
2011-08-13 17:50:58 +0800

    add more methods to test if number is Prime

--- src/main/java/tw/idv/haocheng/exam1/Utils.java
+++ src/main/java/tw/idv/haocheng/exam1/Utils.java
@@ -50,6 +50,64 @@ public class Utils {
 		}
 	}
 
+	public static Integer findMedianWithoutSort(List<Integer> ints) {
+		int medianCandidate = 0;
+		int offset = 1;
+
+		while (true) {
+			int count = 0;
+			count = loopThrough(ints, medianCandidate, count);
+
+			int total = count * 2;
+			System.err.println("median candidate: " + medianCandidate);
+			if (total > medianCandidate + offset) {
+				medianCandidate = (total + medianCandidate) / 2;
+			} else if (total < medianCandidate - offset) {
+				medianCandidate = (total + medianCandidate) / 2;
+			} else if (total <= medianCandidate + offset
+					|| total >= medianCandidate - offset) {
+				System.err.println("total: " + total);
+				return total;
+			}
+		}
+	}
+
+	private static int loopThrough(List<Integer> ints, int medianCandidate,
+			int count) {
+		for (Integer int1 : ints) {
+			if (int1.intValue() > medianCandidate) {
+				count++;
+			}
+		}
+		return count;
+	}
+
+	public static boolean isMersennePrime(int n) {
+		int primes55[] = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43,
+				47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109,
+				113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179,
+				181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241,
+				251, 257 };
+
+		for (int i = 0; i < 55; i++) {
+			if (n % primes55[i] == 0) {
+				if (n == primes55[i]) {
+					return true;
+				} else {
+					return false;
+				}
+			}
+		}
+
+		int maxtest = n / 16;
+
+		for (int i = 259; i < maxtest; i += 2)
+			if (n % i == 0)
+				return false;
+
+		return true;
+	}
+
 	public static boolean isPrime(int n) {
 		if (n < 2)
 			return false;
@@ -61,6 +119,24 @@ public class Utils {
 		for (long i = 6L; i <= sqrtN; i += 6) {
 			if (n % (i - 1) == 0 || n % (i + 1) == 0)
 				return false;
+		}
+		return true;
+	}
+
+	public static boolean isPrime2(int n) {
+		if (n <= 1) {
+			return false;
+		}
+		if (n == 2) {
+			return true;
+		}
+		if (n % 2 == 0) {
+			return false;
+		}
+		for (int i = 3; i <= Math.sqrt(n) + 1; i = i + 2) {
+			if (n % i == 0) {
+				return false;
+			}
 		}
 		return true;
 	}
--- src/test/java/tw/idv/haocheng/exam1/UtilsTest.java
+++ src/test/java/tw/idv/haocheng/exam1/UtilsTest.java
@@ -1,11 +1,17 @@
 package tw.idv.haocheng.exam1;
 
+import static org.junit.Assert.*;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Random;
 
 import org.junit.Test;
 
@@ -27,9 +33,28 @@ public class UtilsTest {
 
 		assertEquals(3.0d, Utils.findMedian(ints).doubleValue(), 1e-15);
 
-		List<Integer> ints2 = Arrays.asList(1, 3, 5, 7);
+		List<Integer> ints2 = Arrays.asList(7, 3, 5, 1);
 
 		assertEquals(4.0d, Utils.findMedian(ints2).doubleValue(), 1e-15);
+	}
+
+	@Test
+	public void findMedianWithoutSort() {
+		List<Integer> ints = Arrays.asList(1, 3, 5);
+
+		assertEquals(2.0d, Utils.findMedianWithoutSort(ints).doubleValue(),
+				1e-15);
+
+		List<Integer> ints2 = Arrays.asList(7, 2, 3, 4, 9, 1, 5, 3);
+
+		assertEquals(4.0d, Utils.findMedianWithoutSort(ints2).doubleValue(),
+				1e-15);
+
+		List<Integer> ints3 = Arrays.asList(7, 2, 3, 4, 9, 1, 5, 3, 10, 12, 5,
+				8, 9, 99, 100, 2);
+
+		assertEquals(4.0d, Utils.findMedianWithoutSort(ints3).doubleValue(),
+				1e-15);
 	}
 
 	@Test
@@ -37,12 +62,89 @@ public class UtilsTest {
 		assertTrue(Utils.isPrime(3));
 		assertTrue(Utils.isPrime(17));
 		assertTrue(Utils.isPrime(31));
-		assertTrue(Utils.isPrime(97));
+		assertTrue(Utils.isPrime(997));
+		assertTrue(Utils.isPrime(1378663747));
 
 		assertFalse(Utils.isPrime(10));
 		assertFalse(Utils.isPrime(9));
 		assertFalse(Utils.isPrime(99));
 		assertFalse(Utils.isPrime(999));
+		assertFalse(Utils.isPrime(1378663748));
+	}
+
+	@Test
+	public void isPrime2() throws Exception {
+		assertTrue(Utils.isPrime2(3));
+		assertTrue(Utils.isPrime2(17));
+		assertTrue(Utils.isPrime2(31));
+		assertTrue(Utils.isPrime2(997));
+		assertTrue(Utils.isPrime2(1378663747));
+
+		assertFalse(Utils.isPrime2(10));
+		assertFalse(Utils.isPrime2(9));
+		assertFalse(Utils.isPrime2(99));
+		assertFalse(Utils.isPrime2(999));
+		assertFalse(Utils.isPrime2(1378663748));
+	}
+
+	@Test
+	public void isMersennePrime() throws Exception {
+		assertTrue(Utils.isMersennePrime(3));
+		assertTrue(Utils.isMersennePrime(17));
+		assertTrue(Utils.isMersennePrime(31));
+		assertTrue(Utils.isMersennePrime(997));
+
+		assertFalse(Utils.isMersennePrime(10));
+		assertFalse(Utils.isMersennePrime(9));
+		assertFalse(Utils.isMersennePrime(99));
+		assertFalse(Utils.isMersennePrime(999));
+	}
+
+	@Test
+	public void compareIsPrimeAlgorithms() throws Exception {
+
+		Random random = new Random();
+		int[] randoms = new int[1000];
+		for (int i = 0; i < 1000; i++) {
+			randoms[i] = random.nextInt(Integer.MAX_VALUE);
+		}
+
+		long start = new Date().getTime();
+		for (int i = 0; i < randoms.length; i++) {
+			int num = randoms[i];
+			BigInteger integer = new BigInteger(String.valueOf(num));
+			System.out.println(integer);
+			System.out.println(integer.isProbablePrime(10));
+		}
+		long end = new Date().getTime();
+		System.err.println("BigInteger.isProbablePrime(10) time spent: "
+				+ (end - start));
+
+		start = new Date().getTime();
+		for (int i = 0; i < randoms.length; i++) {
+			int num = randoms[i];
+			System.out.println(num);
+			System.out.println(Utils.isPrime(num));
+		}
+		end = new Date().getTime();
+		System.err.println("isPrime time spent: " + (end - start));
+
+		start = new Date().getTime();
+		for (int i = 0; i < randoms.length; i++) {
+			int num = randoms[i];
+			System.out.println(num);
+			System.out.println(Utils.isPrime2(num));
+		}
+		end = new Date().getTime();
+		System.err.println("isPrime2 time spent: " + (end - start));
+
+		start = new Date().getTime();
+		for (int i = 0; i < randoms.length; i++) {
+			int num = randoms[i];
+			System.out.println(Utils.isMersennePrime(num));
+		}
+		end = new Date().getTime();
+		System.err.println("isMersennePrime time spent: " + (end - start));
 	}
 
 }
